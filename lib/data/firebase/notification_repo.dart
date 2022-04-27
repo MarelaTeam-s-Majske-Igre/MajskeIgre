acb4119e05ab19176ca49c7c585559faaac502ca
JakMar17 <jakob.marusic17@gmail.com>
2022-04-27 23:23:09 +0200

    new release

--- lib/data/firebase/notification_repo.dart
+++ lib/data/firebase/notification_repo.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:app/data/interceptors/fcm_token_interceptor.dart';
 import 'package:app/data/repo_consts.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:http_interceptor/http/http.dart';
@@ -27,19 +28,39 @@ class NotificationRepo {
 
   Future<void> login({String? token}) async {
     token ??= await getFcmToken();
+
+    final deviceInfo = DeviceInfoPlugin();
+    final androidInfo = await deviceInfo.androidInfo;
+
+    Map<String, dynamic> model = {
+      "androidId": androidInfo.androidId,
+      "brand": androidInfo.brand,
+      "device": androidInfo.device,
+      "fingerprint": androidInfo.fingerprint,
+      "manufacturer": androidInfo.manufacturer,
+      "model": androidInfo.model,
+      "androidRelease": androidInfo.version.release,
+      "androidSkdInt": androidInfo.version.sdkInt,
+      "androidSecurityPatch": androidInfo.version.securityPatch
+    };
+
     Map<String, String> headers = {
       'Content-type': 'application/json',
       'FCM-token': token
     };
 
     try {
-      var response =
-          await client.post(Uri.parse(baseUrl + "login"), headers: headers);
+      var response = await client.post(
+        Uri.parse(baseUrl + "login"),
+        body: json.encode(model),
+        headers: headers,
+      );
 
       while (response.statusCode == 308) {
         if (response.headers.containsKey("location")) {
           response = await client.post(
             Uri.parse(response.headers["location"]!),
+            body: json.encode(model),
             headers: headers,
           );
         }
